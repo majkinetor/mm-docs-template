2f69ad3b2d5713006410476b1528566fd7a93b5d
Miodrag Milic <miodrag.milic@gmail.com>
2021-01-24 23:27:00 +0100

    ..

--- docs.build.ps1
+++ docs.build.ps1
@@ -52,14 +52,16 @@ task Stop {
     } else { Write-Host "No documentation container found serving content" }
 }
 
-# Synopsis: Check internal and external links
-task CheckLinks  {
+# Synopsis: Check internal links
+task CheckLinks Stop, {
+    $Env:MM_DOCS_URL_PREFIX = $script:url
     $ContainerName = "$ContainerName-links"
     docker-run mkdocs serve --dev-addr $ServeAddress -Detach -Expose
     Wait-For "http://localhost:$aPort"
-    $ContainerName = "$ContainerName-blc"
-    docker-run /bin/bash -c "set -o pipefail; blc -eorf --filter-level 0 --exclude .xlsx --exclude .docx --exclude .pdf --exclude .json http://localhost:$aPort/"
-}, Stop
+    exec {
+        docker exec -t $ContainerName /bin/bash -c "set -o pipefail; blc -erf --filter-level 0 http://localhost:$aPort"
+    }
+}
 
 # Synopsis: Clean generated documentation files (not docker images)
 task Clean { remove source\site, source\__pycache__ }
